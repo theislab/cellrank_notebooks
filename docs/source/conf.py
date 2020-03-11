400e12da8baf4a719f14b4255f8ddbcd2bf08e5d
Michal Klein <michal.klein@tngtech.com>
2020-03-12 00:55:44 +0100

    Update tutorial
    Former-commit-id: 4ca1cc32426554b9ab9d6af7052f54ae2f2cec46 [formerly 836b1c3b6c2ab20f337f3a348264fe20ef23d64c]
    Former-commit-id: 81ca1452d2309641af92e7365cedd2d5c10cbb00

---
+++ docs/source/conf.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+import sys
+
+HERE = Path(__file__).parent
+sys.path.insert(0, str(HERE.parent.parent))
+import cellrank  # noqa
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "CellRank"
+copyright = "2019, Marius Lange, Michal Klein, Juan Luis Restrepo Lopez"
+author = "Marius Lange, Michal Klein, Juan Luis Restrepo Lopez"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "nbsphinx",
+    "sphinx_copybutton"
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+source_suffix = [".rst", ".ipynb"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["**.ipynb_checkpoints"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+pygments_style = "sphinx"
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_theme_options = dict(navigation_depth=4, logo_only=True)
+html_context = dict(
+    display_github=True,  # Integrate GitHub
+    github_user="theislab",  # Username
+    github_repo="cellrank_notebooks",  # Repo name
+    github_version="master",  # Version
+    conf_py_path="/docs/source/",
+)  # Path in the checkout to the docs root
+# html_show_sphinx = False
+
+
+def setup(app):
+    app.add_stylesheet("css/custom.css")
