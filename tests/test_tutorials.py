ef116eb28a9e3ae5eebe8fec11eb4d88eb12906d
michalk8 <michal.klein@protonmail.com>
2020-07-04 16:20:47 +0200

    Add sentinel
    Former-commit-id: 6215e57dbfca5efedd5e30a40005f8f2e58fc2c9 [formerly d09e64f5722d8e3213bbe1dd0a572f3a77722be5]
    Former-commit-id: 5114ec9562925e55972e1282350a0c9e3f47bd2a

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,12 +1,56 @@
 # -*- coding: utf-8 -*-
+import json  # noqa
+import tempfile
 from pathlib import Path
+from typing import Callable
+
+import nbformat
+from jsonpath_ng import jsonpath  # noqa
+from jsonpath_ng.ext import parse
 
 TUTORIALS = Path("tutorials").resolve()
+SENTINEL = "SENTINEL REACHED"
 
 
-def test_pancreas_basic(nb_regression):
-    nb_regression.check(str(TUTORIALS / "pancreas_basic.ipynb"))
+def _inject_sentinel(nb) -> None:
+    sentinel = sentinel = nbformat.v4.new_code_cell(source=f"print('{SENTINEL}')")
+    nb["cells"].append(sentinel)
 
 
-def est_pancreas_advanced(nb_regression):
-    nb_regression.check(str(TUTORIALS / "pancreas_advanced.ipynb"))
+def _assert_execute_sentinel(result) -> None:
+    match = parse(
+        f'$.[*].diff[*].diff[*].diff[*].valuelist[?(@.text = "{SENTINEL}\n")].text'
+    )
+    found = match.find(result.diff_filtered)
+
+    if not found:
+        raise RuntimeError(result.diff_string)
+
+
+def test():
+    def wrapper(func: Callable) -> Callable:
+        def decorator(nb_regression) -> None:
+            fpath = f"{(TUTORIALS / func.__name__[5:]).resolve()}.ipynb"
+
+            nb = nbformat.read(fpath, as_version=4)
+            _inject_sentinel(nb)
+
+            with tempfile.NamedTemporaryFile("w", suffix=".ipynb") as tmpf:
+                nbformat.write(nb, tmpf)
+                result = nb_regression.check(tmpf.name, raise_errors=False)
+
+            _assert_execute_sentinel(result)
+
+        return decorator
+
+    return wrapper
+
+
+@test()
+def test_pancreas_basic():
+    pass
+
+
+@test()
+def test_pancreas_advanced():
+    pass
