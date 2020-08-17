d1899be8f04e8af9ccb8548f4c189e729bc24d23
michalk8 <michal.klein@protonmail.com>
2020-08-17 12:18:49 +0200

    Update .travis.yml
    Former-commit-id: e13e00e7df6222648c0451fb629dc0adf9b106b1 [formerly 29cac6d873d3e63a8535417d4bc0d8287916cd68]
    Former-commit-id: 94e9076bdc7a438f4455fd22abd1ca9f5c89f4ee

---
+++ .scripts/ci/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+git clone https://github.com/theislab/cellrank
+cd cellrank
+
+if [[ "$TRAVIS_OS_NAME" == "linux" && "$USE_SLEPC" == "true"]]; then
+    pip install -e".[krylov,test]"
+else
+    pip install -e".[test]"
+fi
+
+python-vendorize
+cd ..
