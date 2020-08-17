a751c6e2cea35b992db6845efcb444f7e9ecd760
michalk8 <michal.klein@protonmail.com>
2020-08-17 12:18:49 +0200

    Update .travis.yml
    Former-commit-id: e13e00e7df6222648c0451fb629dc0adf9b106b1 [formerly 29cac6d873d3e63a8535417d4bc0d8287916cd68]
    Former-commit-id: 94e9076bdc7a438f4455fd22abd1ca9f5c89f4ee

---
+++ .scripts/ci/before_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "linux" && "$USE_SLEPC" == "true"]]; then
+    echo "Installing SLEPc and PETSc dependencies"
+    sudo apt-get update -y
+    sudo apt-get install gcc gfortran libopenmpi-dev libblas-dev liblapack-dev -y
+
+    echo "Installing SLEPc and PETSc"
+
+    sudo pip install mpi4py
+
+    sudo pip install petsc
+    sudo pip install petsc4py
+
+    sudo pip install slepc
+    sudo pip install slepc4py
+
+    python -c "import slepc; import petsc;"
+    echo "Succesfully installed SLEPc and PETSc"
+fi
