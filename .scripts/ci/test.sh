66b5999d15bb48ac5c8195061dafa74e649ee0ee
michalk8 <michal.klein@protonmail.com>
2020-08-27 20:20:04 +0200

    Update travis
    Former-commit-id: 464f98fdd8c8b7dfa1f99e39baf670cbd8b9fcde
    Former-commit-id: b0a6d3f149d0d518f5cbe267ef9cc804e55cc4f3

--- .scripts/ci/test.sh
+++ .scripts/ci/test.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-
-ORIGIN="https://github.com/theislab/cellrank_notebooks"
-
-
 if [[ -n "${DEPLOY_TOKEN+x}" ]]; then
     python -m pytest --regenerate
-    git add tutorials/*
-    git commit -m "[ci skip] Regenerate notebooks"
-    git push "https://$DEPLOY_TOKEN@${ORIGIN:8}"
 else
     python -m pytest
 fi
