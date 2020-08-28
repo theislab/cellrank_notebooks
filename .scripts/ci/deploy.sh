98ada9913c01478cc81e4bfd7450e10438059df2
michalk8 <michal.klein@protonmail.com>
2020-08-28 15:59:58 +0200

    Try fix deployment
    Former-commit-id: 7223689954d4c37d407c0e0b4406aae09fc74c5a

--- .scripts/ci/deploy.sh
+++ .scripts/ci/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-ORIGIN="https://github.com/theislab/cellrank_notebooks"
-
-
 echo "Pushing regenerated notebooks"
-git add -f tutorials/*
-git commit -m "[ci skip] Regenerate notebooks"
-git push "https://$CR_NOTEBOOKS@${ORIGIN:8}"
+
+git config --global user.name "TravisCI"
+
+git add tutorials/*
+git commit --allow-empty -m "[ci skip] Regenerate notebooks"
+git push "https://$CR_NOTEBOOKS@github.com/$TRAVIS_REPO_SLUG" origin master
 
 echo "Requesting documentation rebuild for master"
 curl -X POST -d "branches=master" -d "token=$RTD_TOKEN" https://readthedocs.org/api/v2/webhook/cellrank/125510/
